1c65a797417a33b430a1885737b9d9d87823ad6f
KasiaH <kah.gda@wp.pl>
2024-07-30 10:47:06 +0200

    finished task

---
+++ 01/app.js
@@ -0,0 +1,42 @@
+let a = "4.2";
+let b = 9;
+
+console.log(a, b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+const add = a + b 
+console.log(add);
+
+const substract = a - b 
+console.log(substract);
+
+const multiply = a * b 
+console.log(multiply);
+
+const divide = a / b
+console.log(divide);
+
+
+
+if (add > 20) {
+    console.log("Zmienna jest więsza od 20");
+} else {
+    console.log("Zmienna jest mniejsza od 20");
+}
+if (substract > 20) {
+    console.log("Zmienna jest więsza od 20");
+} else {
+    console.log("Zmienna jest mniejsza od 20");
+}
+if (multiply > 20) {
+    console.log("Zmienna jest więsza od 20");
+} else {
+    console.log("Zmienna jest mniejsza od 20");
+}
+if (divide > 20) {
+    console.log("Zmienna jest więsza od 20");
+} else {
+    console.log("Zmienna jest mniejsza od 20");
+}
